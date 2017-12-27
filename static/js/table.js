32c8ac5f71469f995afb95856915c6c7b225723a
taobupt <wangtaoo665@gmail.com>
2017-12-26 20:37:30 -0600

    first draft

--- static/js/table.js
+++ static/js/table.js
@@ -1,6 +1,6 @@
 'use strict';
 var animateApp = angular.module('animateApp', ['ui.router', 'ngAnimate','ngSanitize','ngProgress']);
-animateApp.config(function ($stateProvider,$urlRouterProvider) {
+animateApp.config(function ($stateProvider,$urlRouterProvider,$sceProvider) {
 
     $urlRouterProvider.otherwise('/home');
     $stateProvider
@@ -14,7 +14,7 @@ animateApp.config(function ($stateProvider,$urlRouterProvider) {
 
 // CONTROLLERS ============================================
 // home page controller
-animateApp.controller('mainController', function($rootScope,$scope,$http,$state,ngProgressFactory) {
+animateApp.controller('mainController', function($rootScope,$scope,$sce,$http,$state,ngProgressFactory) {
 
     $scope.receipt_number = 'YSC1890006200';
     $scope.cases = '1';
@@ -30,7 +30,8 @@ animateApp.controller('mainController', function($rootScope,$scope,$http,$state,
         $scope.progressbar.start();
         $rootScope.queried_cases=[];
         $http({
-                url:'https://arcane-temple-65655.herokuapp.com/query',
+            url:'https://arcane-temple-65655.herokuapp.com/query',
+            //url: 'http://127.0.0.1:5000/query',
                 method:'get',
                 params:{'receipt_number':$('#receipt').val(), 'case_number':$('#case').val()}
             }).then(function (response) {
@@ -39,6 +40,14 @@ animateApp.controller('mainController', function($rootScope,$scope,$http,$state,
                 var results = response.data;
                 for(var i=0; i< results.length;++i){
                     results[i]= JSON.parse(results[i])
+                    if(results[i]['type']=='mailed' || results[i]['type']=='produced'){
+                        results[i]['type'] = '<p style="color:green;">'+ results[i]['type']+"</p>";
+                    }else if(results[i]['type']=='received'){
+                        results[i]['type'] = '<p style="color:blue;">'+ results[i]['type']+"</p>";
+                    }else{
+                        results[i]['type'] = '<p style="color:red;">'+ results[i]['type']+"</p>";
+                    }
+                    results[i]['type']=$sce.trustAsHtml(results[i]['type']);
                 }
                 $rootScope.queried_cases = results;
             })
